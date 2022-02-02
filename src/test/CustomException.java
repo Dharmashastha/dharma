a05f189f58e10176e37ba9aa9233e7c350546f71
Dharmashastha <95913774+Dharmashastha@users.noreply.github.com>
2022-02-02 13:01:40 +0530

    Update CustomException.java

--- src/test/CustomException.java
+++ src/test/CustomException.java
@@ -12,15 +12,4 @@ public CustomException(String message)
 public CustomException(IOException error) {
 	super(error);
 }
-str=str.toLowerCase();
-       boolean letter=true;
-    for(char ch='a';ch <='z';ch++)
-    {
-    if(!str.contains(String.valueof(ch))
-    {
-        letter=false;
-        break;
-    }
-    return letter;
-    }
 }
